94018e0e716488faf76a0571d50a92c995ec4fba
James Min <jmin499@gmail.com>
2019-11-01 16:39:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+  end
+
+  def happiness=(num)
+    if num > 10
+      @happiness = 10
+    end
+  end
+
+  def happiness
+    @happiness
+  end
+  
+
+end
